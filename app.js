3466c53f3a440d634e14c4c11d44561a440cd513
DotSnuk <hampusfarner@outlook.com>
2026-04-30 10:38:48 +0200

    Add requirements and basic functionality

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 'use strict';
+/* import dependencies */
+const express = require('express');
+const jsDOM = require('jsdom');
+const cookieParser = require('cookie-parser');
+const globalObject = require('./servermodules/game-modul.js');
+const fs = require('fs');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use('public/', __dirname + '/static');
+app.get('/', (req, res) => {
+  try {
+  } catch (err) {}
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
 
 //Filen app.js är den enda ni skall och tillåts skriva kod i.
