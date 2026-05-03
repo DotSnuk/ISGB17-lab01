225137588e936e59b3432e75f323c866a3809ef9
DotSnuk <hampusfarner@outlook.com>
2026-05-03 12:55:53 +0200

    Add empty endpoint functions get '/reset' and post '/'

--- app.js
+++ app.js
@@ -11,13 +11,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/public/', express.static(__dirname + '/static'));
 app.use(cookieParser());
 
+app.post('/', (req, res) => {});
+
 app.get('/', (req, res) => {
   try {
     res.sendFile(__dirname + '/static/html/index.html', (err) => {
-      if (err) console.log(err);
+      if (err) throw new Error('Cant open index.html');
     });
-  } catch (err) {}
+  } catch (err) {
+    console.error(err);
+  }
 });
+
+app.get('/reset', (req, res) => {});
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
 
